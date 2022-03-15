fd430b4bd1cd2399e0fa8d1e1c06ac1bed4a9002
mmamorim <marc.m.amorim@gmail.com>
2022-03-15 10:07:24 -0300

    Adicinado: npm, pdfjs

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 
 console.log("Oi gente!");
-console.log("Oi gente!");
-console.log("Oi gente!");
-console.log("Oi gente!");
-console.log("Oi gente!");
+
+const fs = require('fs')
+const pdf = require('pdfjs')
+
+async function geraPdf() {
+    const doc = new pdf.Document({
+        font: require('pdfjs/font/Helvetica'),
+        padding: 10
+    })
+    doc.pipe(fs.createWriteStream('./output.pdf'))
+
+    // render something onto the document
+
+    doc.text('Hello World')
+    doc.text('Hello World')
+    doc.text('Hello World')
+
+    await doc.end()
+}
+
+geraPdf();
